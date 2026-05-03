178403af7aa39fe685b2e6c1f82f0d10f22a2a79
adriandieste <adriandieste2005@gmail.com>
2026-05-03 19:24:21 +0200

    [HU-29] Como Entrenador, quiero poder editar o eliminar un ejercicio personalizado que yo he creado.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Club;
+use App\Models\CustomExercise;
 use App\Policies\ClubPolicy;
+use App\Policies\CustomExercisePolicy;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -26,6 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(Club::class, ClubPolicy::class);
+        Gate::policy(CustomExercise::class, CustomExercisePolicy::class);
 
         Vite::prefetch(concurrency: 3);
 
